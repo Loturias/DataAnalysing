616b4ea0f93309ddff11d682002e44439edced16
Loturias <1351188540@qq.com>
2023-03-02 21:48:19 +0800

    封装CSVLoader类，以后可能会加许多新接口

--- DataLoader/DataLoader.py
+++ DataLoader/DataLoader.py
@@ -3,4 +3,25 @@
 import csv
 
 
+class CSVReader:
+    def __init__(self, FileName,):
+        self.file = open(FileName, 'rt')
+        self.reader = csv.reader(self.file)
+        self.header = next(self.reader)
 
+    def __del__(self):  # For file closing
+        self.file.close()
+
+    def ReadLine(self):  # Return the next line's data from CSV as tuple
+        return tuple(next(self.reader))
+
+
+# Test code
+
+# data = CSVReader("test.csv")
+
+# print(data.header)
+# for rowdata in data.reader:
+#    for info in rowdata:
+#         print(info, end=' ')
+#     print("")
