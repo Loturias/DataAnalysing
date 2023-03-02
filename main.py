b84a50cfc49af72e646eb627f083bb72ba450744
Loturias <1351188540@qq.com>
2023-03-02 22:19:21 +0800

    增加Torch框架信息检查

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # This is the inter subscript of whole project
 
+import os
+
+import DataLoader.DataLoader as Loader
+import LogSys.LogSys as LogSys
+import torch
+
+
+# 初始化PyTorch框架
+LogSys.Print("PyTorch Version: "+torch.__version__)
+LogSys.Print("CUDA Version: "+torch.version.cuda)
+LogSys.Print("Is CUDA available: " + str(torch.cuda.is_available()))
+LogSys.Print("GPU Device Info: " + torch.cuda.get_device_name(0))
